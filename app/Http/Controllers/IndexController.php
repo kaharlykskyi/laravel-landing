a81c03bec8ea5c65cdb5b42c28ac5edb220c3f76
Misha Kaharlykskyi <mishakagar@gmail.com>
2017-10-16 23:48:47 +0300

    models created, views refactoring

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\{Page, Service, Portfolio, People};
 
 class IndexController extends Controller
 {
     public function execute()
     {
-        return view('layouts.site');
+        $pages = Page::all();
+        $services = Service::all();
+        $portfolio = Portfolio::all();
+        $peoples = People::take(3)->get();
+
+        return view('site.index');
     }
 }
